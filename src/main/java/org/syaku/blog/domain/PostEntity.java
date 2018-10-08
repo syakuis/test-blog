e7cd02707d8b94ac36b3daf9ee164c21fbf8d510
syaku <syaku@naver.com>
2018-10-08 20:30:23 +0900

    최초

---
+++ src/main/java/org/syaku/blog/domain/PostEntity.java
@@ -0,0 +1,4 @@
+package org.syaku.blog.domain;
+
+public class PostEntity {
+}
---
+++ src/test/java/org/syaku/blog/SpringBootTest.java
@@ -0,0 +1,18 @@
+package org.syaku.blog;
+
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import static org.junit.Assert.assertEquals;
+
+@RunWith(SpringRunner.class)
+@org.springframework.boot.test.context.SpringBootTest
+public class SpringBootTest {
+
+  @Test
+  public void test() {
+    assertEquals("", "");
+  }
+}
